71de2407ad5902147464dadc182dd09b463d05a8
Anne <anchardo@users.noreply.github.com>
2017-01-16 18:19:29 +0100

    Create pgcc-search.py

---
+++ pgcc-search.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# This parses PIWIK log files and searches for user queries. Written in the context of the BELSPO MADDLAIN project
+# https://www.maddlain.iminds.be/en/home/
+
+# Simon Hengchen - shengche@ulb.ac.be - http://homepages.ulb.ac.be/~shengche
+# Anne Chardonnens - anchardo@ulb.ac.be - http://homepages.ulb.ac.be/~anchardo
+# Raphaël Hubain - rhubain@gmail.com  
+
