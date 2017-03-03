1343c5daeaf781dd2817c2da38c7b5f29d1e7b2f
Anne <anchardo@users.noreply.github.com>
2017-03-03 18:20:34 +0100

    Delete pgcc-search.py

--- pgcc-search.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-# This parses PIWIK log files and searches for user queries. Written in the context of the BELSPO MADDLAIN project
-# https://www.maddlain.iminds.be/en/home/
-
-# Simon Hengchen - shengche@ulb.ac.be - http://homepages.ulb.ac.be/~shengche
-# Anne Chardonnens - anchardo@ulb.ac.be - http://homepages.ulb.ac.be/~anchardo
-# Raphaël Hubain - rhubain@gmail.com  
-
